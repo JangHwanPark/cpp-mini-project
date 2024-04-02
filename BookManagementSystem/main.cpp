a7fc34c27d8916516c7c1ee4075c223ae97a8f34
JangHwanPark <wkd5878@gmail.com>
2024-04-02 23:09:11 +0900

    도서 관리 프로그램 도서 삭제 함수 구현

--- BookManagementSystem/main.cpp
+++ BookManagementSystem/main.cpp
@@ -10,15 +10,33 @@ struct Book {
     int price;
 };
 
+// [Test]객체 출력을 위한 연산자 오버로딩
+ostream& operator<<(ostream& os, const Book& book) {
+    os << "Book Number: " << book.bookNumber
+       << ", Name: " << book.bookName
+       << ", Author: " << book.author
+       << ", Publisher: " << book.publisher
+       << ", Price: " << book.price;
+    return os;
+}
+
 // 메인 화면
 void printMainDisplay() {
-    cout << "========== 도서 관리 시스템 ==========" << "\n";
-    cout << "1. 책 정보 입력\n" << "2. 책 정보 수정\n" << "3. 책 검색\n" << "4. 데이터 삭제\n" << "5. 프로그램 종료" << "\n";
-    cout << "===================================" << "\n";
+    string title = "========== 도서 관리 시스템 ==========";
+    string menu =
+            "1. 책 정보 입력\n"
+            "2. 책 정보 수정\n"
+            "3. 책 검색\n"
+            "4. 데이터 삭제\n"
+            "5. 프로그램 종료\n";
+    string bottomLine = string(title.length(), '=');
+    cout << title << "\n"
+         << menu
+         << bottomLine << "\n";
 }
 
 // 책 정보 입력 함수
-void inputBookInfo() {
+void inputBookInfo(vector<Book> book) {
     cout << "이름";
     cout << "가격";
     cout << "저자";
@@ -26,46 +44,65 @@ void inputBookInfo() {
     cout << "가격";
 }
 
-// 입력 받은 책 정보를 저장하는 함수
-Book saveBookInfo(vector<Book>& books, Book& newBook) {
+// 입력 받은 책 정보를 저장하는 함수 (Book)
+void saveBookInfo(vector<Book>& books, Book& newBook) {
     books.push_back(newBook);
-
-    // Todo : 벡터 내부에 저장된 구조체 데이터를 한꺼번에 모두 출력하기
-    cout << "========================" << "\n" << "입력된 도서 정보" << "\n" << "========================" << "\n";
-    cout << books[0].bookNumber << ". " << books[0].bookName << "\n";
-    cout << books[0].author << "\n";
-    cout << books[0].publisher << "\n";
-    cout << books[0].price << "\n";
-    cout << "========================" << "\n";
+    cout << "입력하신 정보가 저장되었습니다." << "\n";
 }
 
-// Todo : 검색 기능 (return 해야함)
-void getBookInfo(const vector<Book>& books) {
-    cout << "검색 기능" << "\n";
-    string dummySearch = "The Great Gats2by";
-    cout << "TestValue: " << dummySearch << "\n";
+// const Book* 은 포인터가 가리키는 Book 객체를 상수로 취급함을 의미
+const Book* searchBookInfo(const vector<Book>& books) {
+    string dummySearch = "The Great Gatsby";
+    // cout << "const Book& books: " << books[0] << "\n";
 
-    // 문자열 - KMP
-    for (const auto& book : books) {
-        cout << "검색 실행중" << book.bookName;
-        if (book.bookName == dummySearch) cout << book.bookName;
-        else cout << "입력한 도서가 존재하지 않습니다.";
+    // const type& .. - 참조된 객체를 변경할 수 없게 만듬 (읽기만 가능, 데이터 무결성 보장)
+    for (const Book& book : books) {
+        if (book.bookName == dummySearch) {
+            cout << "검색 결과: " << book.bookName << "\n";
+            return &book;
+        }
     }
-    // 번호 - 이진탐색
+
+    cout << "입력한 도서가 존재하지 않습니다." << "\n";
+    return nullptr;
 }
 
-// Todo : 책 정보 수정
-void setBookInfo() {
+// 입력된 정보를 수정하는 함수
+// Book Fn() {} - 실행 될때마다 값으로 반환 (반환시 복사본 생성)
+// Book* Fn() {} - 값의 포인터를 반환. (객체 포인터 반환x, 동적 할당 객체/전역, 정적 범위 객체 해당)
+void modifyBookInfo(vector<Book>& books) {
+    string dummyQuery = "The Great Gatsby";
+    string searchQuery;
 
+    for (Book& book : books) {
+        cout << "수정할 정보를 선택하세요.";
+        cout << "1. 책 이름 변경";
+        cout << "2. 저자 변경";
+        cout << "3. 출판사 변경";
+        cout << "4. 가격 변경";
+        // getline(cin, searchQuery);
+        if (book.bookName == dummyQuery) {
+            cout << "변경할 책 이름을 입력 하세요.";
+        } else {
+            cout << "잘못된 속성 입니다.";
+        }
+        cout << "도서 정보가 수정되었습니다.";
+    }
 }
-/**
- * 1. 수정할 책 검색 로직 작성 (공통 로직으로 사용할 듯)
- * 2. 데이터 수정 로직 작성
- */
+
+void deleteBook(vector<Book>& books) {
+    string dummySearch = "The Great Gatsby";
+
+    if (books[0].bookName == dummySearch) {
+        cout << "\"" << books[0].bookName << "\"" << " 도서가 삭제되었습니다." << "\n";
+        books.pop_back();
+    } else cout << "입력한 도서가 존재하지 않습니다." << "\n";
+}
 
 // 실행 로직
 void run(vector<Book> book, Book dummyBook) {
     while (true) {
+        printMainDisplay();
         int keyword;
         cin >> keyword;
 
@@ -79,6 +116,7 @@ void run(vector<Book> book, Book dummyBook) {
                 break;
             case 3:
                 cout << "검색";
+                searchBookInfo(book);
                 break;
             case 4:
                 cout << "데이터 삭제";
@@ -105,6 +143,13 @@ int main() {
     // printMainDisplay();
     // run(book, dummyBook);
     saveBookInfo(book, dummyBook);
-    getBookInfo(book);
+    cout << "\n";
+    searchBookInfo(book);
+    cout << "\n";
+    deleteBook(book);
+    cout << "\n";
+    searchBookInfo(book);
+    cout << "\n";
+    modifyBookInfo(book);
     return 0;
 }
